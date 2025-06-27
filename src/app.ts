470176ef56f3d1317fe4d1de020f1c7cc334bfb2
Thomas Silva de Farias <thomasfarias19@gmail.com>
2025-06-27 19:23:30 -0300

    initial setuo

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import fastify from "fastify";
+import { ZodError } from "zod";
+import { env } from "./env";
+
+export const app = fastify();
+
+app.setErrorHandler((error, _request, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: "Validation error.", issues: error.format() });
+  }
+
+  if (env.NODE_ENV !== "production") {
+    console.error(error);
+  } else {
+    // TODO: Here we should log to an external tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: "Internal server error." });
+});
